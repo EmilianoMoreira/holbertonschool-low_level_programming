b8c39f4e1743abbe67b2fc2f8d00d5800d9172e7
EmilianoMoreira <Emilianomoreira10152@gmail.com>
2025-02-14 16:29:46 +0000

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - seguimos con printf
+ *
+ * Return. 0
+ */
+
+int main(void)
+{
+sizeof(char)
+sizeof(int)
+sizeof(long int)
+sizeof (long long int)
+sizeof(float)
+
+return (0);
+}
