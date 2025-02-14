d7b02445e7b5854d158d068e4980902c98b59cfe
EmilianoMoreira <Emilianomoreira10152@gmail.com>
2025-02-14 16:31:25 +0000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-sizeof(char)
-sizeof(int)
-sizeof(long int)
-sizeof (long long int)
-sizeof(float)
+sizeof(char);
+sizeof(int);
+sizeof(long int);
+sizeof (long long int);
+sizeof(float);
 
 return (0);
 }
