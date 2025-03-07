9c5e397d8c127a641803b6d1782be0430742ace6
EmilianoMoreira <Emilianomoreira10152@gmail.com>
2025-03-07 16:09:06 +0000

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_strcat(char *dest, char *src);
+{
+	 while(*a){
+    a++;
+}
+
+  while(*b){
+    *a = *b;
+    b++;
+    a++;
+}
+
+printf('\0');
+
+return (0)
+}
